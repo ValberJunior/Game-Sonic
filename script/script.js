373f23331f11175be4254bf016602305a51fd2a8
ValberJunior <valberjr1992@gmail.com>
2021-10-25 19:22:18 -0300

    upload

---
+++ script/script.js
@@ -0,0 +1,202 @@
+const player = document.querySelector('.player');
+const background = document.querySelector('.background');
+const home = document.querySelector('.home');
+const content = document.querySelector('.content');
+const soundToggle = document.querySelector('#soundToggle');
+const score = document.querySelector('#score');
+const record = document.querySelector('#record');
+
+//audios
+const audio1 = document.querySelector('#intro');
+const audio2 = document.querySelector('#jump');
+const audio3 = document.querySelector('#died');
+
+let isJumping = false;
+let position = 0;  //player position 
+
+
+//score
+let highScore = 0;
+let totalScore = 0;
+let actualScore = 0;
+
+//Do have Data?
+let thereIsData = ()=>{
+if (highScore > 0 ){
+ return true;
+ } else{
+  return false;
+}
+}
+
+
+const SCORE_PARCIAL = setInterval(() => {
+    totalScore++
+    score.innerText = totalScore
+    actualScore = totalScore;
+    return actualScore;
+    
+},200)
+
+
+function stopScore(){
+    localStorage.setItem('actualScore',actualScore);
+    clearInterval(SCORE_PARCIAL)
+    isRecord();
+}
+
+
+
+ function isRecord (){
+
+    if (!thereIsData)
+    {
+    localStorage.setItem('recordScore', actualScore);
+    return highScore = localStorage.getItem('recordScore');
+    }
+    else{
+        let scoreData = localStorage.getItem('recordScore');
+            if(actualScore >= scoreData){
+           return localStorage.setItem('recordScore', actualScore);
+        }
+    }
+ }
+
+
+function updateScreen(){
+  
+    record.innerText = localStorage.getItem('recordScore');
+}
+
+
+function handleKeyUp(event){
+
+    if(event.keyCode === 32){
+
+        if(!isJumping){
+        jump();
+        }
+    }
+
+}
+
+function jump(){
+    
+    isJumping = true;
+
+    let upInterval = setInterval(()=>{
+        if(position >= 260){
+            clearInterval(upInterval);
+
+            //Down
+            let downInterval = setInterval(()=>{
+                if(position <=0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }else{
+                position -= 40;
+                player.style.bottom = `${position}px`;
+            }
+            }, 30);
+        }
+        
+        else{
+
+            //up
+            audio2.play();
+            position += 40;
+             player.style.bottom = `${position}px`;
+            }
+        }, 30);
+}
+
+function createObstacle(){
+    const obstacle = document.createElement('img');
+    let obstaclePosition = window.innerWidth - 120;
+    let randomTime =  Math.random()*3000 + Math.random()*3000;
+    
+
+    obstacle.classList.add('obstacle');
+    obstacle.style.left = 1000 + 'px';
+    obstacle.src='../assets/obstacle.gif'
+    background.appendChild(obstacle);
+
+    //movin
+
+
+    let leftInterval = setInterval(()=>{
+   
+        if (obstaclePosition <= -obstaclePosition){
+            clearInterval(leftInterval);
+            background.removeChild(obstacle);
+        } else if( obstaclePosition > 0 && obstaclePosition < 60 && position < 60){
+        
+            //gameover
+            audio3.play();
+            stopScore();
+        
+            // isRecord();
+            setTimeout(()=>{
+            clearInterval(leftInterval);
+            document.body.innerHTML = `<div class='gameOver'> <h1>GAME OVER</h1><h3>SCORE: ${actualScore}</h3><button onclick='restart(this)'>RESTART</button></div>`;
+            },250);
+
+        }else{
+            obstaclePosition -= 10;   //obstacle speed , for change the game level.
+            obstacle.style.left = `${obstaclePosition}px`;    
+        }
+    }, 20)
+
+    setTimeout(createObstacle, randomTime);
+
+  
+
+
+}
+
+
+
+document.addEventListener('keyup', handleKeyUp);
+
+//Events
+
+function restart (){
+    window.location.reload()
+}
+
+function start(){
+    home.style.display ='none';
+    content.style.display = 'block';
+    createObstacle();
+    audio1.play();
+}
+
+
+//sound Toggle
+
+let activateSound = true;
+
+function sound(){
+    let action = !activateSound;
+
+    if(!action){
+        soundToggle.innerHTML = '<i class="fas fa-volume-mute"></i>'
+        audio1.pause();
+        return activateSound = false;
+    }else{
+        soundToggle.innerHTML = '<i class="fas fa-volume-up"></i>'
+        audio1.play();
+        return activateSound = true;
+    }
+
+}
+
+
+
+
+updateScreen()
+
+
+
+
+
